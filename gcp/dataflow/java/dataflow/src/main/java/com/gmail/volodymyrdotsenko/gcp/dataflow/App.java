8fc2d1e0d3ffc2e8634914a77bb262c2a60db171
volodymyrd <volodymyrdotsenko@gmail.com>
2018-06-07 23:21:27 +0200

    rabase dataflow project

---
+++ gcp/dataflow/java/dataflow/src/main/java/com/gmail/volodymyrdotsenko/gcp/dataflow/App.java
@@ -0,0 +1,10 @@
+package com.gmail.volodymyrdotsenko.gcp.dataflow;
+
+/**
+ * Main app.
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Start app..");
+    }
+}
